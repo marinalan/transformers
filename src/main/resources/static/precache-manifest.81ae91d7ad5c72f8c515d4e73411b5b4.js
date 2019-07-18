d02fd075c89def62a5a632fae8ce9df45e6c58b0
Marina Landisberg, Valuality Technologies Limited Git-Admin <mlandisberg@gmail.com>
2019-07-17 23:15:54 -0700

    Adding frontend - compressed built react files

---
+++ src/main/resources/static/precache-manifest.81ae91d7ad5c72f8c515d4e73411b5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbdcac0f77e9ef20fe4157d6e926b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "782a08d169ac5bc9a096",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0a3b1d2fbe645afb026c",
+    "url": "/static/css/main.65643285.chunk.css"
+  },
+  {
+    "revision": "782a08d169ac5bc9a096",
+    "url": "/static/js/2.70e7c17e.chunk.js"
+  },
+  {
+    "revision": "0a3b1d2fbe645afb026c",
+    "url": "/static/js/main.c9c674ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
